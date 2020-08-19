022aa0471b5d5a57fd5f3566309aec13832994ea
Yota Hamada <yohamta@gmail.com>
2020-08-20 01:20:24 +0900

    run the game on ios

---
+++ mobile/ios/godanmaku/MobileEbitenViewControllerWithErrorHandling.swift
@@ -0,0 +1,16 @@
+//
+//  MobileEbitenViewControllerWithErrorHandling.swift
+//  godanmaku
+//
+//  Created by Yota Hamada on 2020/08/20.
+//  Copyright © 2020 Yota Hamada. All rights reserved.
+//
+
+import Foundation
+import Mobile
+
+class MobileEbitenViewControllerWithErrorHandling: MobileEbitenViewController {
+    override func onError(onGameUpdate err: Error!) {
+        print(err ?? "OnGameUpdate Error")
+    }
+}
--- mobile/ios/godanmaku/SceneDelegate.swift
+++ mobile/ios/godanmaku/SceneDelegate.swift
@@ -30,11 +30,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        self.ebitenViewContrller().resumeGame()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        self.ebitenViewContrller().suspendGame()
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
@@ -46,6 +48,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+    }
+    
+    func ebitenViewContrller() -> MobileEbitenViewControllerWithErrorHandling {
+        return self.window!.rootViewController as! MobileEbitenViewControllerWithErrorHandling
     }
 
 
