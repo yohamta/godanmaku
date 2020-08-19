147e2d9c406a3ab52ca54a94fd3287c6839f028f
Yota Hamada <yohamta@gmail.com>
2020-08-20 00:40:44 +0900

    fix bug

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"github.com/hajimehoshi/ebiten"
+
 	"github.com/yohamta/godanmaku/danmaku"
 )
 
 func main() {
-	danmaku.NewGame()
+	game, err := danmaku.NewGame()
+	if err != nil {
+		panic(err)
+	}
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
 }
