7826d0c230a90e2e5a3eba8f06115093acc5c4ac
Yota Hamada <yohamta@gmail.com>
2020-08-22 21:52:10 +0900

    remove unnecessary comments

--- danmaku/main.go
+++ danmaku/main.go
@@ -31,22 +31,6 @@ func (g *Game) Update(screen *ebiten.Image) error {
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
 	myPlayer.Draw(screen)
-	// w, h := gophersImage.Size()
-	// op := &ebiten.DrawImageOptions{}
-
-	// // Move the image's center to the screen's upper-left corner.
-	// // This is a preparation for rotating. When geometry matrices are applied,
-	// // the origin point is the upper-left corner.
-	// op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
-
-	// // Rotate the image. As a result, the anchor point of this rotate is
-	// // the center of the image.
-	// op.GeoM.Rotate(float64(g.count%360) * 2 * math.Pi / 360)
-
-	// // Move the image to the screen's center.
-	// op.GeoM.Translate(screenWidth/2, screenHeight/2)
-
-	// screen.DrawImage(gophersImage, op)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
@@ -58,16 +42,6 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 // NewGame creates a game struct
 func NewGame() (*Game, error) {
-	// Decode image from a byte slice instead of a file so that
-	// this example works in any working directory.
-	// If you want to use a file, there are some options:
-	// 1) Use os.Open and pass the file to the image decoder.
-	//    This is a very regular way, but doesn't work on browsers.
-	// 2) Use ebitenutil.OpenFile and pass the file to the image decoder.
-	//    This works even on browsers.
-	// 3) Use ebitenutil.NewImageFromFile to create an ebiten.Image directly from a file.
-	//    This also works on browsers.
-
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Rotate (Ebiten Demo)")
 
