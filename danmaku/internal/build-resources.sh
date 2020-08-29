416a010e938935a40689be09cdec81cf3cc0eaa5
Yota Hamada <yohamta@gmail.com>
2020-08-29 23:00:38 +0900

    add fonts

--- danmaku/internal/build-resources.sh
+++ danmaku/internal/build-resources.sh
@@ -3,13 +3,18 @@ OUTPUT_FILE=
 PACKAGE_NAME=
 VARIABLE_NAME=
 
-DIRECTORY=resources/images
-PACKAGE_NAME=images
+generate () {
+  DIRECTORY=$1
+  PACKAGE_NAME=$2
+  for f in `ls $DIRECTORY`; do
+    echo "processing $f"
+    INPUT_FILE=$DIRECTORY/$f
+    OUTPUT_FILE=$DIRECTORY/${f%.*}.go
+    VARIABLE_NAME=`echo ${f%.*} | tr '[:lower:]' '[:upper:]'`
+    file2byteslice -input $INPUT_FILE -output $OUTPUT_FILE -package $PACKAGE_NAME -var $VARIABLE_NAME
+  done
+}
 
-for f in `ls $DIRECTORY`; do
-  echo "processing $f"
-  INPUT_FILE=$DIRECTORY/$f
-  OUTPUT_FILE=$DIRECTORY/${f%.*}.go
-  VARIABLE_NAME=`echo ${f%.*} | tr '[:lower:]' '[:upper:]'`
-  file2byteslice -input $INPUT_FILE -output $OUTPUT_FILE -package $PACKAGE_NAME -var $VARIABLE_NAME
-done
+generate "resources/images" "images"
+generate "resources/fonts" "fonts"
+
